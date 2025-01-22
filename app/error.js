ef00401de46a1b8f3846adda5e30bf4d79809ea6
asifalam515 <asibulalam515@gmail.com>
2025-01-22 16:51:18 +0600

    updated

--- app/error.js
+++ app/error.js
@@ -4,6 +4,9 @@ const ErrorPage = ({ error, reset }) => {
     <div>
       <h1 className="text-center text-4xl text-red-500">
         <h1>{error.message}</h1>
+        <button className="btn btn-primary" onClick={() => reset()}>
+          Try Again
+        </button>
       </h1>
     </div>
   );
