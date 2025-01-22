f3a082184cf6450fe3f0abca5be6f027d6ec88ea
asifalam515 <asibulalam515@gmail.com>
2025-01-22 10:39:52 +0600

    image added using next Image

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,13 +1,25 @@
+import Image from "next/image";
+
 const GalleryPage = () => {
   return (
     <div>
       <h1 className="text-3xl text-center">Image Optimizations</h1>
-      <h2 className="text-center">Regular Image with Image Tag</h2>
+      <h2 className="text-center text-2xl">Regular Image with Image Tag</h2>
+      <br />
       <img
         className="mx-auto"
         src="https://nextjs.org/api/docs-og?title=Components%20%3CImage%3E"
         alt="basic img"
       />
+      <br />
+      <Image
+        className="mx-auto"
+        src="https://nextjs.org/api/docs-og?title=Components%20%3CImage%3E"
+        alt="next image"
+        width={500}
+        height={500}
+        quality={100}
+      ></Image>
     </div>
   );
 };
